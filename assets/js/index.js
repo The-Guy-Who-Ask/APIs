d6ed441503f6c429c3dea07495e553507dffd876
husain-ujjaini <husainujjainwala78@gmail.com>
2021-10-13 20:28:30 +0530

    this is explanation of APIs

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+// post -> user
+/*
+https://gorest.co.in/public/v1/posts
+id == post id(changeable but has an end)
+user_id == uploader of post(changeable but has an end)
+*/
+
+// comment-> post comment
+/*
+https://gorest.co.in/public/v1/comments
+id == comment id
+post_id == post id in whic h commented
+name == user who commented
+email == email of name
+*/
